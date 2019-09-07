50061e3722c01d82c3f6a69e2a1b498e41ec7a64
Cecil Thomas <kitsunesama21@gmail.com>
2019-09-07 14:07:36 -0600

    fixed something

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :adventures do 
-    resources :places
+    resources :places, only: [:index, :new, :create, :destroy]
   end
   resources :addresses
 end

80a6e9efe7e463f4176e446b63002a4ef2e330ac
Jianyu Li <jianyucode@gmail.com>
2019-09-07 14:09:03 -0600

    null

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :addresses
-  resources :places
-  resources :adventures
+
+  resources :adventures do
+    resources :places
+  end
+
+  resources :places do
+    resources :addresses
+  end
+
 end

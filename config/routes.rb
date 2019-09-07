b4da03634e4af343e0c6e00d381aad0dd93fb6e7
Cecil Thomas <kitsunesama21@gmail.com>
2019-09-07 17:00:30 -0600

    finished up stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'adventures#index'
   resources :adventures do
     resources :places, only: [:index, :new, :create, :destroy, :show, :edit, :update]
   end
